2873194dd4959201741d6abe3773fb17e30be474
wikibootup <wikibootup@gmail.com>
2016-05-12 15:38:55 +0900

    add oh my git install

--- install.sh
+++ install.sh
@@ -3,7 +3,13 @@
 # References
 # - https://github.com/dahlia/nvimrc/blob/master/install.sh
 
-# Link neovim configuration to ~/.config/zsh
+oh_my_zsh=$HOME/.oh-my-zsh
+if [[ ! -d "$oh_my_zsh" ]]
+then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# Link zsh configuration to ~/.config/zsh
 zshrc_name="zhsrc"
 src_zshrc_dir="$(pwd)"
 src_zshrc="$src_zsh_dir/$zshrc_name"
@@ -13,5 +19,5 @@ echo "Zsh configuration directory: $dst_zsh_dir"
 if [[ ! -f "$dst_zshrc" || "$(cat "$src_zshrc")" != "$(cat "$dst_zshrc")" ]]
 then
   rm -f "$dst_zsh_dir"
-  ln -sfi $src_zshrc_dir $dst_zsh_dir
+  ln -sfi "$src_zshrc_dir" "$dst_zsh_dir"
 fi
